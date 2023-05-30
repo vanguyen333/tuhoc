8c13a1d82f85b73ad7e62f5cad14a3fc6c44e40b
vanguyen333 <vanguyen333@yahoo.com>
2023-05-30 16:08:26 -0400

    added Bloglish component

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,10 @@
 import { useState } from "react";
-const Home = () => {
-  const [name, setName] = useState("mario");
-  const [age, setAge] = useState(23);
-  const handleClick = () => {
-    const name = ["Nhan", "Oanh", "Ngoc"];
-    const int = Math.floor(Math.random() * 3);
-    setName(name[int]);
-    setAge(30);
-  };
+import Bloglist from "./Bloglist";
 
+const Home = () => {
   return (
     <div className="home">
-      <h2>HomePage</h2>
-      <p>
-        {name} is {age} years old.
-      </p>
-      <button onClick={handleClick}>Click me</button>
+      <Bloglist />
     </div>
   );
 };
