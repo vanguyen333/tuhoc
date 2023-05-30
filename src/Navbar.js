46beef04e90d2078bd0586e5009395c545a3d5ac
vanguyen333 <vanguyen333@yahoo.com>
2023-05-30 12:08:04 -0400

    added css styling for navbar

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Van's Blog</h1>
+      <a href="/">Home</a>
+      <a href="/Create"> New Blog</a>
+    </nav>
+  );
+};
+
+export default Navbar;
