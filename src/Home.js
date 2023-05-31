98c8c02589a0962f57136330341f3b3e4ee0339b
vanguyen333 <vanguyen333@yahoo.com>
2023-05-31 16:03:23 -0400

    #17 fetch data

--- src/Home.js
+++ src/Home.js
@@ -2,31 +2,24 @@ import { useState, useEffect } from "react";
 import Bloglist from "./Bloglist";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new site", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   const [name, setName] = useState("mario");
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
 
   useEffect(() => {
-    console.log("use effect ran");
-    console.log(name);
-  }, [name]); //the empty array stops the functions it renders over and again
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []); //the empty array stops the functions it renders over and again
 
   return (
     <div className="home">
-      <Bloglist blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      {blogs && <Bloglist blogs={blogs} title="All Blogs" />}
       <button onClick={() => setName("luigi")}>change name</button>
       <p>{name}</p>
     </div>
