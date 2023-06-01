2b682922c80e26368076cc64744650fea16a52d0
vanguyen333 <vanguyen333@yahoo.com>
2023-06-01 11:53:49 -0400

    #30

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
-
+  const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
@@ -15,6 +17,9 @@ const Create = () => {
       body: JSON.stringify(blog),
     }).then(() => {
       console.log("new blog added");
+      setIsPending(false);
+      // history.go(1);
+      navigate.push("/");
     });
   };
 
